f84052717cb453407e3ec92e7118a800923233a2
Alejandro Vidal <alejandro.vidal@aktios.com>
2024-02-24 18:25:07 +0100

    chore: use dynamic viewports units

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ type AuthLayoutProps = {
 
 export default async function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <div className="min-h-screen flex">
+    <div className="min-h-dvh flex">
       <div className="flex items-center justify-center w-full sm:w-1/2 md:w-1/3 flex-shrink-0">
         <div className="flex-1 mx-10">{children}</div>
       </div>
