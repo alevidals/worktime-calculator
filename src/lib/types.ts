a9b6b8ace602d0246485632f38ea72ff8843b168
Alejandro Vidal <alejandro.vidal@aktios.com>
2024-02-24 18:10:44 +0100

    fix: remove wrong import

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,20 +1,5 @@
-import { z } from "zod";
-import { addIssueSchema, loginSchema } from "./schema";
-
-export type Issue = {
-  id: string;
-  name: string;
-  startTime: string;
-  endTime: string;
-  workTime: number;
-};
-
 export type GroupedIssue = {
   name: string;
   workTime: number;
   percentage: number;
 };
-
-// Schemas
-export type AddIssue = z.infer<typeof addIssueSchema>;
-export type Login = z.infer<typeof loginSchema>;
