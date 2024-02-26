605337ff6854d66ac5a1612ef6ca2195a0172490
raskardev <alexcdev95@gmail.com>
2024-02-26 18:48:23 +0100

    feat: redirect to dashboard if logged in

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,21 @@
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+
 type AuthLayoutProps = {
   children: React.ReactNode;
 };
 
 export default async function AuthLayout({ children }: AuthLayoutProps) {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    return redirect("/");
+  }
+
   return (
     <div className="min-h-dvh flex items-center justify-center max-w-72 mx-auto">
       {children}
